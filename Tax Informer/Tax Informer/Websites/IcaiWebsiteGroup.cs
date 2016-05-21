c91960ab0136773ba5a335019bb3ca9e628d91d0
Suman Karan <xpd532@gmail.com>
2016-05-21 22:05:09 +0530

    Icai head office is now fully supported

--- Tax Informer/Tax Informer/Websites/IcaiWebsiteGroup.cs
+++ Tax Informer/Tax Informer/Websites/IcaiWebsiteGroup.cs
@@ -28,12 +28,17 @@ namespace Tax_Informer.Websites
             return new List<ChildWebsite>()
             {
                 new WhatsNewChild(this),
-                new BosAnnouncementChild(this)
+                new BosAnnouncementChild(this),
+                new NewsChild(this),
+                new AnnouncementChild(this),
+                new ExaminationChild(this)
             };
         }
 
         public override Artical ReadArtical(ArticalOverview overview, HtmlDocument doc)
         {
+            var artical = overview.ToArtical();
+
             var container = doc.GetElementbyId("maincontent");
             if (container == null) return null;
 
@@ -44,12 +49,20 @@ namespace Tax_Informer.Websites
             });
             if (contentTable == null) return null;
 
-            return new Artical()
+            artical.HtmlText = contentTable.InnerHtml;
+
+            if(string.IsNullOrEmpty(artical.Title) || string.IsNullOrEmpty(artical.Date))
             {
-                Title = overview.Title,
-                MyLink = overview.LinkOfActualArtical,
-                HtmlText = contentTable.InnerHtml
-            };
+                var titleContainer = Helper.AnyChild(container, "span", "Heading_Inner");
+                if (titleContainer != null)
+                {
+                    string title, date;
+                    getTitleAndDate(HtmlEntity.DeEntitize(titleContainer.InnerText), out title, out date);
+                    artical.Title = title;
+                    artical.Date = date;
+                }                    
+            }
+            return artical;
         }
 
         public override ArticalOverview[] ReadIndexPage(string url, HtmlDocument doc, out string nextPageUrl)
@@ -66,10 +79,15 @@ namespace Tax_Informer.Websites
             foreach (var li in liTags)
             {
                 var aLink = Helper.AnyChild(li, "a");
+
+                string title, date;
+                getTitleAndDate(HtmlEntity.DeEntitize(aLink.InnerText), out title, out date);
+
                 data.Add(new ArticalOverview()
                 {
                     LinkOfActualArtical = Helper.CombindUrl(webDir, aLink.GetAttributeValue("href", "")),
-                    Title = HtmlEntity.DeEntitize(aLink.InnerText)
+                    Title = title,
+                    Date = date
                 });
             }
 
@@ -78,7 +96,6 @@ namespace Tax_Informer.Websites
             {
                 try
                 {
-
                     var currentPageIndex = int.Parse(Helper.AnyChild(pageContainer, "font").InnerText);
                     var allLinks = Helper.AllChild(pageContainer, "a");
 
@@ -92,6 +109,32 @@ namespace Tax_Informer.Websites
             }
 
             return data.Count > 0 ? data.ToArray() : null;
+        }
+
+        private void getTitleAndDate(string value, out string title, out string date)
+        {
+            //Ind AS Transition Facilitation Group (ITFG) Clarification Bulletins - (10-05-2016)
+            //CA curriculum, exam pattern to be revised soon: Reddy - The Sikh Times - Vadodara - (11-Apr-2016)
+            try
+            {
+                var indexOfStartDate = value.LastIndexOf('(');
+
+                var tempTitle = value.Substring(0, indexOfStartDate);
+                title = tempTitle.EndsWith(" - ") ? (tempTitle.Substring(0, tempTitle.Length - 3)) : tempTitle;
+
+                var tempDate = value.Substring(indexOfStartDate).Replace("(", "").Replace(")", "");
+                var ss = tempDate.Split(new string[] { "-" }, StringSplitOptions.RemoveEmptyEntries);
+                int month = -1;
+                if (int.TryParse(ss[1], out month))
+                    date = ss[2] + ss[1] + ss[0];
+                else
+                    date = ss[2] + Helper.GetMonthIndex(ss[1]).ToString("00") + ss[0];
+            }
+            catch (Exception)
+            {
+                title = value;
+                date = null;                
+            }            
         }
 
         public override Artical ReadArticalExtrnal(ArticalOverview overview, string extrnalLink)
@@ -158,6 +201,64 @@ namespace Tax_Informer.Websites
                 }
             }
             public BosAnnouncementChild(WebsiteGroup parent) : base(parent, "Bos") { }
+            public override Artical ReadArtical(ArticalOverview overview, HtmlDocument doc) => Parent.ReadArtical(overview, doc);
+
+            public override ArticalOverview[] ReadCategory(Category category, HtmlDocument doc, out string nextPageUrl) => Parent.ReadIndexPage(category.Link, doc, out nextPageUrl);
+            public override ArticalOverview[] ReadIndexPage(string url, HtmlDocument doc, out string nextPageUrl) => Parent.ReadIndexPage(url, doc, out nextPageUrl);
+            public override Artical ReadArticalExtrnal(ArticalOverview overview, string extrnalLink) => Parent.ReadArticalExtrnal(overview, extrnalLink);
+            protected override void RestoreState(Dictionary<string, object> state) { }
+        }
+
+        private sealed class NewsChild : ChildWebsite
+        {
+            public override string IndexPageLink { get; } = "http://www.icai.org/new_all_news.html";
+            public NewsChild(WebsiteGroup parent) : base(parent, "News") { }
+
+            //TODO: Some articals are now properly viewing in news like http://www.icai.org/news.html?news=7111
+            public override Artical ReadArtical(ArticalOverview overview, HtmlDocument doc) => Parent.ReadArtical(overview, doc);
+
+            public override ArticalOverview[] ReadIndexPage(string url, HtmlDocument doc, out string nextPageUrl) => Parent.ReadIndexPage(url, doc, out nextPageUrl);
+
+            public override Artical ReadArticalExtrnal(ArticalOverview overview, string extrnalLink) => Parent.ReadArticalExtrnal(overview, extrnalLink);
+        }
+
+        private sealed class AnnouncementChild : ChildWebsite
+        {
+            public override string IndexPageLink { get; } = "http://www.icai.org/new_category.html?c_id=219";
+            public AnnouncementChild(WebsiteGroup parent) : base(parent, "Announcements") { }
+
+            public override Artical ReadArtical(ArticalOverview overview, HtmlDocument doc) => Parent.ReadArtical(overview, doc);
+
+            public override ArticalOverview[] ReadIndexPage(string url, HtmlDocument doc, out string nextPageUrl) => Parent.ReadIndexPage(url, doc, out nextPageUrl);
+
+            public override Artical ReadArticalExtrnal(ArticalOverview overview, string extrnalLink) => Parent.ReadArticalExtrnal(overview, extrnalLink);
+
+            protected override void RestoreState(Dictionary<string, object> state) { }
+        }
+
+        private sealed class ExaminationChild : ChildWebsite
+        {
+            public override string IndexPageLink { get; } = "http://www.icai.org/new_category.html?c_id=342";
+            public override Category[] Categories
+            {
+                get
+                {
+                    return new Category[]
+                    {
+                        new Category()
+                        {
+                            Name = "Students",
+                            Link ="http://www.icai.org/new_category.html?c_id=410"
+                        },
+                        new Category()
+                        {
+                            Name = "Members",
+                            Link ="http://www.icai.org/new_category.html?c_id=411"
+                        }
+                    };
+                }
+            }
+            public ExaminationChild(WebsiteGroup parent) : base(parent, "Examination") { }
             public override Artical ReadArtical(ArticalOverview overview, HtmlDocument doc) => Parent.ReadArtical(overview, doc);
 
             public override ArticalOverview[] ReadCategory(Category category, HtmlDocument doc, out string nextPageUrl) => Parent.ReadIndexPage(category.Link, doc, out nextPageUrl);
